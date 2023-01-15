5ed0e4141ee654c6362fff6c7a673db1539dfa19
Lukasz Trojan <lukasztrojan846@gmail.com>
2023-01-15 15:38:28 +0100

    UserDao

--- src/main/java/com/sda/dao/UserDAO.java
+++ src/main/java/com/sda/dao/UserDAO.java
@@ -1,4 +1,19 @@
 package com.sda.dao;
+import com.sda.model.User;
+
+import com.sda.db.HibernateUtils;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 public class UserDAO {
+
+    public void create (User user){
+        try(Session session = HibernateUtils.getSessionFactory().openSession()){
+
+            Transaction transaction = session.beginTransaction();
+            session.persist(user);
+            transaction.commit();
+        }
+
+    }
 }
